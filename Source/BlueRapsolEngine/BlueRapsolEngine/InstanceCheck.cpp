b779d0a7fce90a9088331a53c8233d0a50010e5b
tsuzuri1 <tsuzuri13231360@gmail.com>
2018-09-21 13:32:45 -0400

    multiple instance checker

--- Source/BlueRapsolEngine/BlueRapsolEngine/InstanceCheck.cpp
+++ Source/BlueRapsolEngine/BlueRapsolEngine/InstanceCheck.cpp
@@ -1,10 +1,24 @@
 //Check for multiple Instances
+//File Lab2 section 1
+//Author Tsuzuri Terry Mizore Okada 101095454
+//Created on 2018 -09 -20
+//Updated on 2018 09 - 21
 
 #include "InitBlueRapsol.h" 
 #include <windows.h>
 #include <WinBase.h>
+#include <iostream>
+
+using namespace std;
 
 bool InitBlueRapsol::IsOnlyInstance(LPCTSTR appName) {
-	
+	HANDLE handle = CreateMutex(NULL, TRUE, appName);
+
+	if (GetLastError() != ERROR_SUCCESS) {
+		cout << "Error: An instance of this appication is already running" << endl;
+		return false;
+	}
+	cout << "No other instance of this application is found running" << endl;
+
 	return true;
 }
