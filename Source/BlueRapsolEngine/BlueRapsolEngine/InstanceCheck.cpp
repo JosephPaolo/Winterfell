bd3954ec3d4bead8da431cdebb2a6fe43b53afb8
JosephPaolo <jmalibiran2@georgebrown.ca>
2018-09-21 14:21:42 -0400

    fix instanceCheck 	modified:   Source/BlueRapsolEngine/BlueRapsolEngine/InstanceCheck.cpp

--- Source/BlueRapsolEngine/BlueRapsolEngine/InstanceCheck.cpp
+++ Source/BlueRapsolEngine/BlueRapsolEngine/InstanceCheck.cpp
@@ -1,23 +1,24 @@
 //Check for multiple Instances
+//File Lab2 section 1
+//Author Tsuzuri Terry Mizore Okada 101095454
+//Created on 2018 -09 -20
+//Updated on 2018 09 - 21
 
 #include "InitBlueRapsol.h" 
 #include <windows.h>
 #include <WinBase.h>
-#include <iostream> 
+#include <iostream>
 
 using namespace std;
 
 bool InitBlueRapsol::IsOnlyInstance(LPCTSTR appName) {
-	HANDLE handle = CreateMutex(NULL, TRUE, appName); //attempt to create Mutex
+	HANDLE handle = CreateMutex(NULL, TRUE, appName);
 
-	if (GetLastError() != ERROR_SUCCESS) { //if attempt to create mutex causes error
-		cout << "Error: An instance of this application is already running!" << endl;
+	if (GetLastError() != ERROR_SUCCESS) {
+		cout << "Error: An instance of this appication is already running" << endl;
 		return false;
 	}
+	cout << "No other instance of this application is found running" << endl;
 
-	//if mutex is created successfully, attempts to run another of the same 
-	//program will have then first check for the existance of this created Mutex.
-	//Note: Mutex insure only one of the app can be running on a machine.
-	cout << "No other instance of this application is found running." << endl;
 	return true;
 }
