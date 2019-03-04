9e0631139b100e9f470740425834a81ba2df0b08
Tsuzuri Okada <101095454@georgebrown.ca>
2019-03-04 17:10:37 -0500

    added on audio

--- Source/BlueRapsolEngine/BlueRapsolEngine/AudioSystem.h
+++ Source/BlueRapsolEngine/BlueRapsolEngine/AudioSystem.h
@@ -1,7 +1,16 @@
 #pragma once
 
 #include <SFML/Audio.hpp>
+#include <windows.h>
 
 class AudioSystem {
+private:
+	sf::SoundBuffer buffer;
+	sf::Sound sound;
 
+public:
+	AudioSystem();
+	void LoadBuffer();
+	void SoundPlay();
+	void SoundStop();
 };
